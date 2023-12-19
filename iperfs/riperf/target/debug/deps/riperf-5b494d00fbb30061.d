79e8fca94ea6f425faf57d37e2ca5a7c99b0293b
Sunjae <sunjaekim0228@gmail.com>
2023-12-19 09:32:14 +0000

    Ignore riperf/target/

--- iperfs/riperf/target/debug/deps/riperf-5b494d00fbb30061.d
+++
@@ -1,5 +0,0 @@
-/home/sunj/cnet/croffset/iperfs/riperf/target/debug/deps/riperf-5b494d00fbb30061: src/main.rs
-
-/home/sunj/cnet/croffset/iperfs/riperf/target/debug/deps/riperf-5b494d00fbb30061.d: src/main.rs
-
-src/main.rs:
--- iperfs/riperf/target/debug/riperf.d
+++
@@ -1,1 +0,0 @@
-/home/sunj/cnet/croffset/iperfs/riperf/target/debug/riperf: /home/sunj/cnet/croffset/iperfs/riperf/src/main.rs
