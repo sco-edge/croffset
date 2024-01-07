ef8dce61edb4254d5047546ac54fb7f03ead65de
Sunjae <sunjaekim0228@gmail.com>
2024-01-07 09:53:13 +0000

    Sync load.py

--- iperfs/load.py
+++ iperfs/load.py
@@ -9,12 +9,13 @@ def main():
     num_flows = int(args.flow)
     time = int(args.time)
     cca = "bbr"
-    tag = ""
 
-    if not tag:
-        experiment = f"rx-h-f{num_flows}-t{time}-{cca}-0"
+    global experiment
+
+    if args.vxlan:
+        experiment = f"rx-e-f{num_flows}-t{time}-{cca}-0"
     else:
-        experiment = f"rx-h-{tag}-f{num_flows}-t{time}-{cca}-0"
+        experiment = f"rx-h-f{num_flows}-t{time}-{cca}-0"
 
     (util_p, util_f) = start_cpu_utilization()
     interrupt_f = start_interrupt_count()
@@ -37,8 +38,8 @@ def main():
     os.mkdir(experiment)
     os.chdir(experiment)
 
-    end_cpu_utilization(util_p, util_f, experiment)
-    post_process_interrupt_count(interrupt_f, experiment)
+    end_cpu_utilization(util_p, util_f)
+    post_process_interrupt_count(interrupt_f)
 
 def start_cpu_utilization():
     f = tempfile.NamedTemporaryFile()
@@ -46,7 +47,7 @@ def start_cpu_utilization():
 
     return (p, f)
 
-def end_cpu_utilization(p, f, experiment):
+def end_cpu_utilization(p, f):
     p.kill()
     with open(f'cpu.{experiment}.out', 'w') as cpu_output:
         subprocess.run(["../../iperfs/cpu.py", f.name], stdout=cpu_output)
@@ -60,7 +61,7 @@ def start_interrupt_count():
 
     return f
 
-def post_process_interrupt_count(old_f, experiment):
+def post_process_interrupt_count(old_f):
     new_f = tempfile.NamedTemporaryFile()
     subprocess.run(["cat", "/proc/interrupts"], stdout=new_f)
 
@@ -75,6 +76,7 @@ if __name__ == "__main__":
     parser.add_argument('--flow', '-f', default=6)
     parser.add_argument('--time', '-t', default=60)
     parser.add_argument('--silent', '-s', action='store_true')
+    parser.add_argument('--vxlan', '-v', action='store_true')
 
     global args
     args = parser.parse_args()
