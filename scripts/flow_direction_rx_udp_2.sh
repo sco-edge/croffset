d4159dac553d20414c726460490bc8531b076256
Sunjae <sunjaekim0228@gmail.com>
2024-07-08 08:42:15 +0000

    fix: affinity scripts for Intel Flow Director

--- scripts/flow_direction_rx_udp_2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ethtool -U ens801f0 flow-type udp4 src-ip 192.168.2.102 dst-ip 192.168.2.103 dst-port 8472 action 2
-
--- scripts/flow_direction_tx_tcp.sh
+++ scripts/flow_direction_tx_tcp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 42000 action 2
-ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 42001 action 3
-ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 42002 action 4
-ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 42003 action 5
-ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 42004 action 6
-ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 42005 action 7
-ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 42006 action 8
-ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 42007 action 9
+ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 45000 action 2
+ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 45001 action 3
+ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 45002 action 4
+ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 45003 action 5
+ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 45004 action 6
+ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 45005 action 7
+ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 45006 action 8
+ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 45007 action 9
 
 ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 43000 action 2
 ethtool -U ens801f0 flow-type tcp4 src-ip 192.168.2.103 dst-ip 192.168.2.102 dst-port 43001 action 3
