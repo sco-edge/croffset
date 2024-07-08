2c18561ef7c8fac4ba2119e245521d0603284556
Sunjae <sunjaekim0228@gmail.com>
2024-07-08 08:39:42 +0000

    feat: Add some plots

--- plot/sr/plot-sr-bbr.py
+++ plot/sr/plot-sr-bbr.py
@@ -43,7 +43,7 @@ def generate_plot_data(data):
         extype = f"h{i}-c0"
         runs = data[extype]
         throughput = np.mean([run.throughput for run in runs])
-        retrans = np.mean([run.retrans for run in runs])
+        retrans = np.mean([run.retrans / 10 for run in runs])
         spurious = np.mean([run.spurious_retrans for run in runs])
         
         norm_srs = []
@@ -97,35 +97,40 @@ def configure_pp():
     # pp.set_cmap("Dark2")
     # colors = pp.cm.hot(np.linspace(0,1,10))
     # pp.gca().set_prop_cycle(cycler('color', colors))
-    pp.rcParams["figure.figsize"] = (3.4, 1.15)
+    pp.rcParams["figure.figsize"] = (3.4, 1.16)
     pp.rcParams["font.family"] = "Inter"
-    pp.rcParams["font.size"] = 6
+    pp.rcParams["font.size"] = 8
+    pp.rcParams["hatch.linewidth"] = 0.75
 
 def plot(host, cont, name):
     x = np.array([1, 2, 3, 4, 5, 6])
 
     fig, ax1 = pp.subplots()
     p1,  = ax1.plot(x, [item.throughput for item in host], linewidth=1, marker='o', markersize=3, label='Host throughput')
-    p2,  = ax1.plot(x, [item.throughput for item in cont], color='C1', linewidth=1, marker='D', markersize=3, label='Container throughput')
+    p2,  = ax1.plot(x, [item.throughput for item in cont], linewidth=1, marker='x', markersize=3, label='Container throughput')
 
-    ax1.set_xlabel('Number of flows')
+    ax1.set_xlabel('BBR flows')
     ax1.set_ylabel('Throughput (Gbps)')
     ax1.set_xticks([1, 2, 3, 4, 5, 6])
     ax1.set_yticks([0, 20, 40, 60, 80, 100])
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    p3 = ax2.bar(x - 0.15, [item.retrans for item in host], 0.3, label='Host retrans')
+    # p3 = ax2.bar(x - 0.15, [item.retrans for item in host], 0.3, label='Host retrans')
     p4 = ax2.bar(x - 0.15, [item.spurious for item in host], 0.3, label='Host spurious')
-    p5 = ax2.bar(x + 0.15, [item.retrans for item in cont], 0.3, label='Container retrans')
-    p6 = ax2.bar(x + 0.15, [item.spurious for item in cont], 0.3, label='Container spurious')
+    # p5 = ax2.bar(x + 0.15, [item.retrans for item in cont], 0.3, label='Container retrans')
+    p6 = ax2.bar(x + 0.15, [item.spurious for item in cont], 0.3, label='Container spurious', hatch='////')
+    # p6 = ax2.bar(x + 0.15, [item.spurious for item in cont], 0.3, label='Container spurious')
 
-    ax2.set_ylabel('Number of Spurious Retx.')
-    ax2.set_yticks([0, 1000, 2000, 3000, 4000], ["0", "1k", "2k", "3k", "4k"])
+    ax2.set_ylabel('Spurious retx.')
+    ax2.set_yticks([0, 150, 300, 450, 600, 750], ["0", "150", "300", "450", "600", "750"])
 
     pp.savefig(f"{name}.png", dpi=300, bbox_inches='tight', pad_inches=0.03)
+    pp.savefig(f"{name}.eps", dpi=300, bbox_inches='tight', pad_inches=0.03)
+    pp.savefig(f"{name}.svg", dpi=300, bbox_inches='tight', pad_inches=0.03)
 
 if __name__ == "__main__":
     rack = {}
+    reno = {}
 
     parse_dataset(rack, "../../dataset/noin-bbr-20240415")
     rack.pop("h0-c3")
@@ -133,15 +138,28 @@ if __name__ == "__main__":
     rack.pop("h0-c5")
     rack.pop("h0-c6")
     parse_dataset(rack, "../../dataset/noin-bbr-20240415-compl")
+    rack.pop("h0-c2")
+    rack.pop("h0-c3")
     rack.pop("h0-c4")
     rack.pop("h0-c5")
     rack.pop("h0-c6")
+    parse_dataset(rack, "../../dataset/noin-bbr-20240417-c2")
+    parse_dataset(rack, "../../dataset/noin-bbr-20240418-c3")
     parse_dataset(rack, "../../dataset/noin-bbr-20240417-c4")
     parse_dataset(rack, "../../dataset/noin-bbr-20240417-c5")
     parse_dataset(rack, "../../dataset/noin-bbr-20240417-c6")
+    rack.pop("h0-c6")
+    parse_dataset(rack, "../../dataset/noin-bbr-20240419-c6")
+    # rack.pop("h0-c5")
+    # parse_dataset(rack, "../../dataset/noin-bbr-20240419-c5")
 
     for key in rack:
-        print(key, len(rack[key]))
+        if len(rack[key]) > 20:
+            print(f"{key} has {len(rack[key])} items")
+
+    for key in reno:
+        if len(rack[key]) > 20:
+            print(f"{key} has {len(reno[key])} items")
 
     (rack_host, rack_cont) = generate_plot_data(rack)
 
--- plot/sr/plot-sr-cubic.py
+++ plot/sr/plot-sr-cubic.py
@@ -43,7 +43,7 @@ def generate_plot_data(data):
         extype = f"h{i}-c0"
         runs = data[extype]
         throughput = np.mean([run.throughput for run in runs])
-        retrans = np.mean([run.retrans for run in runs])
+        retrans = np.mean([run.retrans / 10 for run in runs])
         spurious = np.mean([run.spurious_retrans for run in runs])
         
         norm_srs = []
@@ -92,36 +92,80 @@ def configure_pp():
     for font_file in font_files:
         mpl.font_manager.fontManager.addfont(font_file)
 
+    pp.rcParams["axes.prop_cycle"] = pp.cycler("color", pp.cm.Dark2.colors)
+
+    # pp.set_cmap("Dark2")
+    # colors = pp.cm.hot(np.linspace(0,1,10))
+    # pp.gca().set_prop_cycle(cycler('color', colors))
+    pp.rcParams["figure.figsize"] = (3.4, 1.16)
     pp.rcParams["font.family"] = "Inter"
+    pp.rcParams["font.size"] = 8
+    pp.rcParams["hatch.linewidth"] = 0.75
 
 def plot(host, cont, name):
     x = np.array([1, 2, 3, 4, 5, 6])
 
     fig, ax1 = pp.subplots()
-    p1,  = ax1.plot(x, [item.throughput for item in host], color='C0', linewidth=2, marker='o', markersize=4, label='Host throughput')
-    p2,  = ax1.plot(x, [item.throughput for item in cont], color='C1', linewidth=2, marker='o', markersize=4, label='Container throughput')
-    # yrange = np.array([0, 2])
-    # pp.ylim(yrange)
+    p1,  = ax1.plot(x, [item.throughput for item in host], linewidth=1, marker='o', markersize=3, label='Host throughput')
+    p2,  = ax1.plot(x, [item.throughput for item in cont], linewidth=1, marker='x', markersize=3, label='Container throughput')
+
+    ax1.set_xlabel('CUBIC flows')
+    ax1.set_ylabel('Throughput (Gbps)')
+    ax1.set_xticks([1, 2, 3, 4, 5, 6])
+    ax1.set_yticks([0, 20, 40, 60, 80, 100])
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    p3 = ax2.bar(x - 0.2, [item.retrans for item in host], 0.4, label='Host retrans')
-    p4 = ax2.bar(x - 0.2, [item.spurious for item in host], 0.4, label='Host spurious')
-    p5 = ax2.bar(x + 0.2, [item.retrans for item in cont], 0.4, label='Container retrans')
-    p6 = ax2.bar(x + 0.2, [item.spurious for item in cont], 0.4, label='Container spurious')
+    # p3 = ax2.bar(x - 0.2, [item.retrans for item in host], 0.4, label='Host retrans')
+    p4 = ax2.bar(x - 0.2, [item.spurious for item in host], 0.3, label='Host spurious')
+    # p5 = ax2.bar(x + 0.2, [item.retrans for item in cont], 0.4, label='Container retrans')
+    p6 = ax2.bar(x + 0.2, [item.spurious for item in cont], 0.3, label='Container spurious', hatch='/////')
 
-    pp.savefig(f"{name}.png", dpi=300, bbox_inches='tight', pad_inches=0.05)
+    ax2.set_ylabel('Spurious retx.')
+    ax2.set_yticks([0, 150, 300, 450, 600, 750], ["0", "150", "300", "450", "600", "750"])
+    # box = ax1.get_position()
+    # ax1.set_position([box.x0, box.y0 + box.height * 0.1,
+    #                 box.width, box.height * 0.9])
+
+    # Put a legend below current axis
+    # ax1.legend([p1, p2, p3, p4, p5, p6],
+    #            [p1.get_label(), p2.get_label(), p3.get_label(), p4.get_label(), p5.get_label(), p6.get_label()],
+    #             loc='upper center', bbox_to_anchor=(0.5, -0.3), ncol=3)
+    # ax1.legend([p1, p2, p4, p6],
+    #            [p1.get_label(), p2.get_label(), p4.get_label(), p6.get_label()],
+    #             loc='upper center', bbox_to_anchor=(0.5, -0.3), ncol=2)
+
+
+    pp.savefig(f"{name}.png", dpi=300, bbox_inches='tight', pad_inches=0.03)
+    pp.savefig(f"{name}.eps", dpi=300, bbox_inches='tight', pad_inches=0.03)
 
 if __name__ == "__main__":
     rack = {}
 
-    parse_dataset(rack, "../../dataset/noin-bbr-20240415")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240413")
+    rack.pop("h1-c0")
+    rack.pop("h2-c0")
+    rack.pop("h3-c0")
+    rack.pop("h4-c0")
+    rack.pop("h5-c0")
+    rack.pop("h6-c0")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-h1")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-h2")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-h3")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-h4")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-h5")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-h6")
+    rack.pop("h0-c1")
+    rack.pop("h0-c2")
     rack.pop("h0-c3")
     rack.pop("h0-c4")
     rack.pop("h0-c5")
     rack.pop("h0-c6")
-    parse_dataset(rack, "../../dataset/noin-bbr-20240415-compl")
-    rack.pop("h0-c4")
-    parse_dataset(rack, "../../dataset/noin-bbr-20240417-c4")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-c1")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-c2")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-c3")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-c4")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-c5")
+    parse_dataset(rack, "../../dataset/noin-cubic-20240419-c6")
 
     for key in rack:
         print(key, len(rack[key]))
@@ -130,4 +174,4 @@ if __name__ == "__main__":
 
     configure_pp()
 
-    plot(rack_host, rack_cont, "bbr-rack")
+    plot(rack_host, rack_cont, "cubic-rack")
